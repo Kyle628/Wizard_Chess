61837421a67c6e49aa25c3385aa2599f88c809b9
kyle <kyjoconn@ucsc.edu>
2017-03-31 17:20:41 -0700

    piece class done, working on Board

---
+++ Piece.js
@@ -0,0 +1,21 @@
+class Piece {
+    constructor(color, chessman, square) {
+        this.color = color;
+        this.chessman = chessman;
+        this.square = square;
+        }
+
+    getColor() {
+        return this.color;
+    }
+
+    getChessman() {
+        return this.chessman;
+    }
+
+    getSquare() {
+        return this.square;
+    }
+
+
+}
---
+++ board.js
@@ -0,0 +1,66 @@
+class Board {
+    // create a chess board with columns a-h and rows 1-8
+    constructor() {
+        this.squares = {"a": {"1":new Piece("white","rook", "a1"), "2":new Piece("white","pawn", "a2"), "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                    "b": {"1":null, "2":null, "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                    "c": {"1":null, "2":null, "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                    "d": {"1":null, "2":null, "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                    "e": {"1":null, "2":null, "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                    "f": {"1":null, "2":null, "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                    "g": {"1":null, "2":null, "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                    "h": {"1":null, "2":null, "3":null, "4":null, "5":null, "6":null, "7":null, "8":null},
+                }
+    }
+
+    // get the piece that is on any square
+    getPiece() {
+        if (square === null) {
+            return null;
+        }
+        var col = square.charAt(0);
+        var row = square.charAt(1);
+        return this.squares[col][row];
+    }
+
+    //
+    updateSquare(endSquare, piece) {
+        var startingSquare = piece.getSquare();
+        var startCol = startingSquare.charAt(0);
+        var startRow = startingSquare.charAt(1);
+        var endCol = endSquare.charAt(0);
+        var endRow = endSquare.charAt(1);
+        // move piece to end square
+        this.squares[endCol][endRow] = piece;
+        // update the old square
+        this.squares[startCol][startRow] = null;
+        return
+    }
+
+    updateBoard() {
+        var currSquare = null;
+        var currCol = null;
+        var currRow = null;
+        for (var col in this.squares) {
+            currCol = col;
+            for (var row in this.squares[col]) {
+                currRow = row;
+                currSquare = currCol.concat(currRow);
+                //has the piece moved?
+                // check if space is empty
+                if (this.squares[col][row] == null) {
+                    continue;
+                }
+                else if (this.squares[col][row].getSquare() != currSquare) {
+                    console.log(this.squares[col][row].getSquare());
+                    // Move the piece
+                    this.updateSquare(this.squares[col][row].getSquare(), this.squares[col][row]);
+                }
+            }
+        }
+    }
+}
+
+var board = new Board();
+board.updateSquare("e5", board.squares["a"]["1"]);
+
+board.updateBoard();
