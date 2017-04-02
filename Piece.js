3f4a22c224f04850aaba9d125c3ee521111613dc
kyle <kyjoconn@ucsc.edu>
2017-04-01 17:00:12 -0700

    pawns can move one space forward, backward, or diagonal. no check yet for if the square is empty

--- Piece.js
+++ Piece.js
@@ -1,8 +1,14 @@
+function colToNum(col) {
+    col = parseInt(col.charCodeAt(0) - 96);
+    return col;
+}
+
 class Piece {
-    constructor(color, chessman, square) {
+    constructor(color, chessman, square, hasMoved) {
         this.color = color;
         this.chessman = chessman;
         this.square = square;
+        this.hasMoved = hasMoved;
         }
 
     getColor() {
@@ -17,7 +23,51 @@ class Piece {
         return this.square;
     }
 
+    colToNum(col) {
+        col = parseInt(col.charCodeAt(0) - 96);
+        return col;
+    }
+
+    movePawn(square) {
+        var col = square.charAt(0);
+        var row = square.charAt(1);
+        var newSquare = col.concat(row);
+        var startCol = colToNum(this.square.charAt(0));
+        var startRow = this.square.charAt(1);
+        var endCol = colToNum(square.charAt(0));
+        var endRow = square.charAt(1);
+        if (!this.hasMoved) {
+            console.log("hasnt moved");
+            console.log(Math.abs(parseInt(startRow) - parseInt(endRow)));
+            if (Math.abs(parseInt(startRow) - parseInt(endRow)) <= 2 &&
+                Math.abs(parseInt(startCol) - parseInt(endCol)) == 0) {
+                    this.square = newSquare;
+                    console.log(this.square);
+                } else if (Math.abs(parseInt(startRow) - parseInt(endRow)) == 1 &&
+                    Math.abs(parseInt(startCol) - parseInt(endCol)) < 2) {
+                        this.square = newSquare;
+                        console.log(this.square);
+                    } else {
+                        console.log("invalid pawn move");
+                    }
+                return;
+            return;
+        } else {
+            if (Math.abs(parseInt(startRow) - parseInt(endRow)) == 1 &&
+                Math.abs(parseInt(startCol) - parseInt(endCol)) < 2) {
+                    this.square = newSquare;
+                    console.log(this.square);
+                } else {
+                    console.log("invalid pawn move");
+                }
+            return;
+        }
+    }
+
     movePiece(square) {
+        if (this.chessman == "pawn") {
+            this.movePawn(square);
+        }
         var col = square.charAt(0);
         var row = square.charAt(1);
         var newSquare = col.concat(row);
@@ -27,3 +77,6 @@ class Piece {
 
 
 }
+
+var piece = new Piece("white", "pawn", "a3", true);
+piece.movePiece("b4");
